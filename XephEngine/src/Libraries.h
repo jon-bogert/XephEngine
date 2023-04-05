90b61b5d5d447841cdf97b5d310b2187603284a1
jon-bogert <jonbogert@gmail.com>
2023-04-04 19:51:03 -0700

    New From Scratch
    Basic Solution and Folder Structure

--- XephEngine/src/Libraries.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <SFML/Main.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-
-#include <vector>
-#include <iostream>
--- XephEngine/src/systems/Math.cpp
+++
@@ -1,7 +0,0 @@
-#include "Math.h"
-
-xe::Vector2 xe::Math::ForwardVector(float theta)
-{
-	theta *= 3.14159 / 180;
-	return Vector2(sinf(theta), cosf(theta));
-}
